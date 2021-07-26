6d16efa7fd5e87f56e5937b33e1dfbb4f5440507
Ambiorix <pabloa.bonilla@outlook.com>
2021-07-25 21:54:34 -0400

    map animated

--- script.js
+++ script.js
@@ -10,31 +10,25 @@ const api_key = "at_El5SrP6wCszI4dj874vVBx5cneD8Q";
 const APIURL = 'https://geo.ipify.org/api/';
 let currentVersion = 'v1';
 
+var map = L.map('mapid').setView([40.823358, -73.943825], 16);
 
-const map = L.map('mapid', {
-    'center': [0,0],
-    'zoom': 0,
-    'layers': [
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoianV0c3VlZCIsImEiOiJja3JoM2ozcXYwOHp2MnBwZW1tcWw2eTZsIn0.feGZBEd6Xiw3aeEpta_KPw', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>, Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>, Coded by <a href="https://github.com/Jutsued" target="_blank">Pablo A Bonilla</a>',
-            id: 'mapbox/streets-v11',
-            accessToken: 'pk.eyJ1IjoianV0c3VlZCIsImEiOiJja3JoM2ozcXYwOHp2MnBwZW1tcWw2eTZsIn0.feGZBEd6Xiw3aeEpta_KPw',
-            tileSize: 512,
-            zoomOffset: -1,
-          })
-    ]
-})
 
-updateMarker = (update_marker = [40.823358, -73.943825]) => {
-     map.setView(update_marker, 13);
-     L.marker(update_marker).addTo(map);
-}
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoianV0c3VlZCIsImEiOiJja3JoM2ozcXYwOHp2MnBwZW1tcWw2eTZsIn0.feGZBEd6Xiw3aeEpta_KPw', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>, Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>, Coded by <a href="https://github.com/Jutsued" target="_blank">Pablo A Bonilla</a>',
+  id: 'mapbox/streets-v11',
+  accessToken: 'pk.eyJ1IjoianV0c3VlZCIsImEiOiJja3JoM2ozcXYwOHp2MnBwZW1tcWw2eTZsIn0.feGZBEd6Xiw3aeEpta_KPw',
+  tileSize: 512,
+  zoomOffset: -1,
+}).addTo(map);
 
-// let update_marker = l.icon ({
-//     iconUrl: './images/icon-location.svg',
-//     iconSize: [34, 44],
-//     iconAnchor: [40.823358, -73.943825]
-// });
+var update_marker = L.icon ({
+    iconUrl: './images/icon-location.svg',
+    iconSize: [34, 44],
+    iconAnchor: [23, 56],
+});
+
+L.marker([40.823358, -73.943825], {icon: update_marker}).addTo(map);
+
 
 getIPDetails = (default_ip) => {
     if(default_ip == undefined) {
@@ -50,7 +44,9 @@ getIPDetails = (default_ip) => {
         timezone.innerHTML = "UTC" + data.location.timezone;
         isp.innerHTML = data.isp;
 
-        updateMarker([data.location.lat, data.location.lng])
+        L.marker([data.location.lat, data.location.lng], {icon: update_marker}).addTo(map);
+        map.flyTo([data.location.lat, data.location.lng], 16)
+        
     })
     .catch(error => {
         alert("Unable to get IP details")
@@ -58,7 +54,7 @@ getIPDetails = (default_ip) => {
     })
 }
 
-document.addEventListener('load', updateMarker());
+document.addEventListener('load', update_marker);
 
 btn.addEventListener('click', e => {
     e.preventDefault()
